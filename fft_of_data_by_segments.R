2689a409035d374698781638d1a52acec0654217
ssamsonau <ssamsonau@gc.cuny.edu>
2014-11-10 18:26:36 -0500

    second submission fft 200 SVM linear kernel

--- fft_of_data_by_segments.R
+++ fft_of_data_by_segments.R
@@ -1,128 +1,126 @@
-folder <- "Patient_1"
-path <- paste0("e:/Temp/", folder)
-fft_comp_n <- 100
-#N.of.clusters <- 8 # Number of clusters for a parralel execution
+fft_comp_n <- 200
+#N.of.clusters <- 2 # Number of clusters for a parralel execution
+data_location <- "j:/Data_Epil/"
 
 ## All variables shoulbe be entered above _______________
-
 library(Revobase);   setMKLthreads(4)
 
 #library(foreach); library(doParallel)
 #workers <- makeCluster(N.of.clusters);  registerDoParallel(workers)
 
 library(R.matlab)
+library(data.table)
 
-ptm <- proc.time()
+write("", file="./fft_progress.txt", append=FALSE)
 
-total_positive <- length(grep("preictal", dir(path))) 
-total_negative <- length(grep("interictal", dir(path)) )
-total_test <- length(grep("test", dir(path))) 
+#for(folder in dir(data_location)){
+for(folder in c("Patient_2")){
 
+    #folder <- "Dog_1"
+    path <- paste0(data_location, folder)
+    
+    ptm <- proc.time()
+    
+    total_positive <- length(grep("preictal", dir(path))) 
+    total_negative <- length(grep("interictal", dir(path)) )
+    total_test <- length(grep("test", dir(path))) 
+    
     # Read first file to determine number of sensors
-filename <- paste0(folder, "_", "preictal", "_segment_", "0001", ".mat")
-pathname <- file.path(path, filename)    
-data_temp <- R.matlab::readMat(pathname)
-mat_temp <- as.matrix(data_temp[[1]][[1]][,])  # the data
-N.of.sensors <- nrow(mat_temp)
-
-#Data table for input data
-MAT <- data.frame( matrix(data = NA, nrow = total_positive+total_negative+total_test,
-             ncol = (2*fft_comp_n) *N.of.sensors) )
-
-MAT$Out <- c(rep(1, total_positive), rep(0, total_negative), rep(NA, total_test) )
-MAT$Type <- "NA"
-MAT$Segment <- NA
-
-library(GeneCycle)
-
-write("", file="./current_progress.txt", append=FALSE)
-
-for(fullCase_cur_number in 1:(total_positive+total_negative+total_test)){
-#foreach(fullCase_cur_number = 1:(total_positive+total_negative+total_test) ) %dopar% {
-#foreach can not work with sideeffects. One should make a separate code to take this into account    
-    ## FORM A FILE NAME TO READ
-    if( fullCase_cur_number <= total_positive ){
-        file_type <- "preictal"
-        file_number <- fullCase_cur_number            
-        MAT[fullCase_cur_number, "Out"] <- 1
-    }
-    else if( fullCase_cur_number <= total_positive+total_negative ){
-        file_type <- "interictal"   
-        file_number <- fullCase_cur_number-total_positive
-        MAT[fullCase_cur_number, "Out"] <- 0
-    }
-    else{
-        file_type <- "test"   
-        file_number <- fullCase_cur_number-(total_positive+total_negative)
-    }
-    
-    MAT[fullCase_cur_number, "Type"] <- file_type
-        
-    file_number_t <- paste0(file_number)
-    
-    if(file_number <= 9)
-        file_number_t <- paste0("000", file_number_t)     
-    else if(file_number <= 99)
-        file_number_t <- paste0("00", file_number_t)
-    else if(file_number <= 999)
-        file_number_t <- paste0("0", file_number_t)            
-    
-    filename <- paste0(folder, "_", file_type, "_segment_", file_number_t, ".mat")
-
-    ## READ FILE
-    cat("Working with file", filename, "\n")
-    write( paste("Working with file", filename, "\n"), "current_progress.txt", append=TRUE)
+    filename <- paste0(folder, "_", "preictal", "_segment_", "0001", ".mat")
     pathname <- file.path(path, filename)    
     data_temp <- R.matlab::readMat(pathname)
     mat_temp <- as.matrix(data_temp[[1]][[1]][,])  # the data
-    if(file_type %in% c("preictal", "interictal"))  
-        # there is no inforamtion about segment for test data
-        MAT[fullCase_cur_number, "Segment"] <- data_temp[[1]][[5]][,] #segment number
+    N.of.sensors <- nrow(mat_temp)
     
-    ## MAKE FFT transformation for data from a given file
-    for(r in 1:N.of.sensors){ # we have N.of.sensors rows in data - one row for every sensor
-        pg <- GeneCycle::periodogram(mat_temp[r, ])
-        s_ord <- order(pg$spec, decreasing = T)
+    #Data table for input data
+    MAT <- data.frame( matrix(data = NA, nrow = total_positive+total_negative+total_test,
+                              ncol = (2*fft_comp_n) *N.of.sensors) )
+    
+    MAT$Out <- c(rep(1, total_positive), rep(0, total_negative), rep(NA, total_test) )
+    MAT$Type <- "NA"
+    MAT$Segment <- NA
+    MAT$file.name <- "NA"
+    
+    #library(GeneCycle)
+    
+    for(fullCase_cur_number in 1:(total_positive+total_negative+total_test)){
+        #foreach(fullCase_cur_number = 1:(total_positive+total_negative+total_test) ) %dopar% {
+        #foreach can not work with sideeffects. One should make a separate code to take this into account    
+        ## FORM A FILE NAME TO READ
+        if( fullCase_cur_number <= total_positive ){
+            file_type <- "preictal"
+            file_number <- fullCase_cur_number            
+            MAT[fullCase_cur_number, "Out"] <- 1
+        }
+        else if( fullCase_cur_number <= total_positive+total_negative ){
+            file_type <- "interictal"   
+            file_number <- fullCase_cur_number-total_positive
+            MAT[fullCase_cur_number, "Out"] <- 0
+        }
+        else{
+            file_type <- "test"   
+            file_number <- fullCase_cur_number-(total_positive+total_negative)
+        }
         
-        cur_MAT_index <- 1 # for a given sensor we fill 
-                           #in the cur_MAT_index place
+        MAT[fullCase_cur_number, "Type"] <- file_type
         
-        for(k in 1:max(s_ord)){ # go through max amplitude values and choose
-                                #only those wich are on top
-            cur_max_number <- which(s_ord==k)
+        file_number_t <- paste0(file_number)
+        
+        if(file_number <= 9)
+            file_number_t <- paste0("000", file_number_t)     
+        else if(file_number <= 99)
+            file_number_t <- paste0("00", file_number_t)
+        else if(file_number <= 999)
+            file_number_t <- paste0("0", file_number_t)            
+        
+        filename <- paste0(folder, "_", file_type, "_segment_", file_number_t, ".mat")
+        MAT[fullCase_cur_number, "file.name"] <- filename
+        
+        ## READ FILE
+        cat("Working with file", filename, "\n")
+        write( paste("Working with file", filename, "\n"), "fft_progress.txt", append=TRUE)
+        pathname <- file.path(path, filename)    
+        data_temp <- R.matlab::readMat(pathname)
+        mat_temp <- as.matrix(data_temp[[1]][[1]][,])  # the data
+        if(file_type %in% c("preictal", "interictal"))  
+            # there is no inforamtion about segment for test data
+            MAT[fullCase_cur_number, "Segment"] <- data_temp[[1]][[5]][,] #segment number
+        
+        ## MAKE FFT transformation for data from a given file
+        for(r in 1:N.of.sensors){ # we have N.of.sensors rows in data - one row for every sensor
+            #pg <- GeneCycle::periodogram(mat_temp[r, ])
+            pg.Mod <- Mod(fft(mat_temp[r, ])) 
+            pg.Mod <- pg.Mod[1:(length(pg.Mod)/2)] # we need only unique half
             
-            if(cur_MAT_index > fft_comp_n) break 
-                #stop loop when needed number of places filled in
-
-            if(cur_max_number == 1 | cur_max_number == length(pg$spec)) next
-                # do not include the point on edge
+            pg.Freq <- (1:length(pg.Mod) ) /length(pg.Mod) #this is not the actual freq scale. But we don't need actual
             
-            # check if this is true maximum
-            if( pg$spec[cur_max_number] > pg$spec[cur_max_number-1] &
-                    pg$spec[cur_max_number] > pg$spec[cur_max_number+1] ){
-                # if yes, save the data
-                MAT[ fullCase_cur_number, (r-1)*(2*fft_comp_n) + cur_MAT_index] <-
-                    pg$spec[cur_max_number] # modes amplitude
-                
-                MAT[ fullCase_cur_number, (r-1)*(2*fft_comp_n) + fft_comp_n + cur_MAT_index] <-
-                    pg$freq[cur_max_number] # correspond. frequencies
+            max_pos <- which(diff(sign(diff( pg.Mod )))==-2)+1
             
-                #print((r-1)*(2*fft_comp_n) + cur_MAT_index)
-                #print((r-1)*(2*fft_comp_n) + fft_comp_n + cur_MAT_index)
-                #print(cur_MAT_index)
-
-                cur_MAT_index = cur_MAT_index + 1
-            }
+            #check
+            #pg.Mod.t <- pg.Mod[1:30]
+            #plot(pg.Mod.t)
+            #max_pos.t <- which(diff(sign(diff( pg.Mod.t )))==-2)+1
+            #points((1:29)[max_pos.t], rep(-2, length(max_pos.t)), col="red")
+            #
+            
+            FFT.DT <- data.table(spec = pg.Mod[max_pos], freq = pg.Freq[max_pos])
+            setkey(FFT.DT, spec)
+            FFT.DT.l <- nrow(FFT.DT)
+            save.FFT <- FFT.DT[FFT.DT.l:(FFT.DT.l - fft_comp_n + 1 )]
+            
+            MAT[ fullCase_cur_number, ((r-1)*(2*fft_comp_n)+1):((r-1)*2*fft_comp_n + fft_comp_n) ] <- save.FFT$spec
+            MAT[ fullCase_cur_number, ((r-1)*2*fft_comp_n+fft_comp_n+1):(r*2*fft_comp_n) ] <- save.FFT$freq
+            
         }
     }
+       
+    # Save the data
+    library(MASS)
+    write.matrix(MAT, file = paste0("fft", fft_comp_n, "_MAT_", folder, "_SegmentsSeparate.csv") , sep = ",")
+    # next step would be To take learning algorithm and apply to data
+    
+    write( proc.time() - ptm, "fft_progress.txt", append=TRUE)
+    cat(proc.time() - ptm)
 }
-
-
-# Save the data
-library(MASS)
-write.matrix(MAT, file = paste0("fft_MAT_", folder, "_SegmentsSeparate.csv") , sep = ",")
-# next step would be To take learning algorithm and apply to data
-
-write( proc.time() - ptm, "current_progress.txt", append=TRUE)
 
 #stopCluster(workers)
--- prediction.R
+++ prediction.R
@@ -1,68 +1,73 @@
-folder <- "Dog_2"
-path <- "j:/Temp/"
-fft_comp_n <- 100
-#N.of.clusters <- 2 # Number of clusters for a parralel execution
+fft_comp_n <- 200
+N.of.clusters <- 2 # Number of clusters for a parralel execution
+data_location <- "j:/Data_Epil/"
 
 ## All variables shoulbe be entered above _______________
+library(Revobase);   setMKLthreads(4)
 
-library(Revobase);   setMKLthreads(2)
+library(foreach); library(doParallel)
+workers <- makeCluster(N.of.clusters);  registerDoParallel(workers)
 
-#library(foreach); library(doParallel)
-#workers <- makeCluster(N.of.clusters);  registerDoParallel(workers)
-
+library(R.matlab)
 library(data.table)
-
-MAT <- fread( paste0("fft_MAT_", folder, "_SegmentsSeparate.csv") , sep = ",") 
-library(stringr)
-MAT <- MAT[, lapply(.SD, str_trim)]
-setkey(MAT, Type)
-
-TrainDT <- MAT[Type==c("preictal", "interictal") , .SD, .SDcols=-c("Out", "Type", "Segment") ]
-TrainDT <- TrainDT[, lapply(.SD, as.numeric)]
-    #name of factor should not start with a number
-Y <- MAT[Type==c("preictal", "interictal") , factor( paste0("P", Out) ) ] 
-
-#TrainDT <- TrainDT[1:50]
-#Y <- Y[1:50]
-
 library(caret); library(pROC)
 
-    # cv does not make sense - for many folds there will be no possitive examples.
-    # from experience cv does not give any reasonable results - Sens high, but Spec only 0     
-    # bootstrapping ... I need to think... In any case boot632 is a better one
-    # boot632 gives reasonable results
+progress_file <- "./Output/training_progress.txt"
+
+base::cat("", file=progress_file, append=F)
+
+for(folder in dir(data_location)){
+    
+    base::cat("reading", folder, "\n", file=progress_file, append=T)
+    MAT <- fread( paste0("./MAT/fft", fft_comp_n , "_MAT_", folder, "_SegmentsSeparate.csv") , sep = ",") 
+    library(stringr)
+    MAT <- MAT[, lapply(.SD, str_trim)]
+    setkey(MAT, Type)
+    
+    TrainDT <- MAT[Type==c("preictal", "interictal") , .SD, 
+                   .SDcols=-c("Out", "Type", "Segment", "file.name") ]
+    TrainDT <- TrainDT[, lapply(.SD, as.numeric)]
+        #name of factor should not start with a number
+    Y <- MAT[Type==c("preictal", "interictal") , factor( paste0("P", Out) ) ] 
+    
+    #TrainDT <- TrainDT[1:50]
+    #Y <- Y[1:50]
     
     # http://stats.stackexchange.com/questions/17602/caret-re-sampling-methods
-ctrl <- trainControl(method="boot632", number=10,
-                     summaryFunction = twoClassSummary, 
-                     classProbs = TRUE, 
-                     verboseIter=T)
-
-trGrid <- expand.grid(C=c(1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1, 1e1, 1e2, 1e3, 1e4))
-#trGrid <- expand.grid(mtry=c(2, 80))
-
-Mod <- train(TrainDT, Y, method="svmLinear", 
-#Mod <- train(TrainDT, Y, method="rf",
-             tuneGrid=trGrid,
-             preProcess=c("center", "scale"), metric= "ROC", 
-             verbose = TRUE,
-             trControl = ctrl )
-
-#Save model if needed
-
-#Make prediction
-TestDT <- MAT[Type==c("test") , .SD, .SDcols=-c("Out", "Type", "Segment") ]
-TestDT <- TestDT[, lapply(.SD, as.numeric)]
-
-Prediction <- predict(Mod, newdata=TestDT )
-
-files_names <- grep("test", dir(paste0(path, folder)), value=T)
-
-#files_names <- files_names[1:50]
-
-Predicted_Data <- cbind(files_names, Prediction)
-
-write.table(Predicted_Data, file=paste0("./Predicted_", folder, ".txt"), quote=FALSE, 
-            row.names=F, col.names = F, append=F, sep=",")
-
-#stopCluster(workers)
+    ctrl <- trainControl(method="boot632", number=10,
+                         summaryFunction = twoClassSummary, 
+                         classProbs = TRUE, 
+                         verboseIter=T)
+    
+    trGrid <- expand.grid(C=c(1e-4, 3e-4, 1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1, 1e1, 1e2, 1e3, 1e4))
+    #trGrid <- expand.grid(C=c(1e-4, 1e-3, 1e-2, 1e-1))
+    #trGrid <- expand.grid(C=c(1e-3))
+    #trGrid <- expand.grid(mtry=c(2, 80))
+    
+    base::cat("training", folder, "\n", file=progress_file, append=T)
+    Mod <- train(TrainDT, Y, method="svmLinear", 
+    #Mod <- train(TrainDT, Y, method="rf",
+                 tuneGrid=trGrid,
+                 preProcess=c("center", "scale"), metric= "ROC", 
+                 verbose = TRUE,
+                 trControl = ctrl )
+    
+    capture.output(Mod, file=progress_file, append=T)
+    #Save model if needed
+    save(Mod, file=paste0("./Output/Predicted_fft_Model", fft_comp_n, "_", folder, ".model"))
+    
+    
+    #Make prediction
+    TestDT <- MAT[Type==c("test"), .SD, .SDcols=-c("Out", "Type", "Segment", "file.name") ]
+    TestDT <- TestDT[, lapply(.SD, as.numeric)]
+    
+    base::cat("prediction process...\n")
+    Prediction <- as.numeric( predict(Mod, newdata=TestDT ) ) -1
+            
+    Predicted_Data <- cbind(MAT[Type==c("test"), file.name], Prediction)
+    
+    write.table(Predicted_Data, file=paste0("./Output/Predicted_fft", fft_comp_n, "_", folder, ".csv"), quote=FALSE, 
+                row.names=F, col.names = F, append=F, sep=",")
+    
+}
+stopCluster(workers)
